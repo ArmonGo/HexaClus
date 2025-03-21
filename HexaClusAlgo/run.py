54d206a2f5c351d17a8901f8ac6767704c5d4da5
ArmonGo <106155841+ArmonGo@users.noreply.github.com>
2025-03-21 19:39:39 +0100

    Update run.py

--- HexaClusAlgo/run.py
+++ HexaClusAlgo/run.py
@@ -5,25 +5,44 @@ from cv import GridSearcher
 from load_data import load_london, load_gdf, load_paris, load_newyork
 
 parameters_grid = { 'Lr_ridge' : {"alpha": np.arange(0.1,1,0.1)},
-                    'clustering' : {"resolutions": [5, 6, 7, 8, 9]}
+                    'clustering' : {"resolutions": [5,6,7,8,9]},
+                    'RDF' :  { "min_samples_split" : [2,3,5],
+                                        "min_samples_leaf" : [3,5,10]}
                     }
-save_path = './result/new_york/'
+citys = ['new_york', 'london', 'paris']
+selected_areas = ["New York City, US", "London, UK", "Paris, FR"]
+load_fs = [load_newyork, load_london, load_paris]
 max_iter = 10000
-patience = 150 
-selected_area = "New York City, US"
+patience = 300 
+ # try to keep more polygons
+thresholds = [5e-05, 5e-05, 0]
 
-df = load_newyork( split_rate=(0.7, 0.1, 0.2), scale =True, coords_only = False)
-df = df.reset_index(drop=True)
-(gdf_train, gdf_val, gdf_test), (df_train, df_val, df_test) = load_gdf(df)
+parameters_grid = { 'Lr_ridge' : {"alpha": np.arange(0.1,1,0.1)},
+                    'clustering' : {"resolutions": [5,6,7,8,9]},
+                    'RDF' :  { "min_samples_split" : [2,3,5],
+                                        "min_samples_leaf" : [3,5,10]}
+                    }
 
-searcher = GridSearcher(parameters_grid['clustering'], save_path = save_path)
-m, rmse, preds, instances = searcher.cv_clustering( gdf_train, gdf_val, gdf_test, max_iter, patience, selected_area)
+def main(parameters_grid, citys, selected_areas, max_iter, patience):
+    for l in range(len(citys)):
+        save_path =  './result/' + citys[l] + '/'
+        l_f = load_fs[l]
+        area = selected_areas[l]
+        threshold = thresholds[l]
+        # load data 
+        df = l_f( split_rate=(0.7, 0.1, 0.2), scale =True, coords_only = False)
+        df = df.reset_index(drop=True)
+        (gdf_train, gdf_val, gdf_test), (df_train, df_val, df_test) = load_gdf(df)
+        # begin search
+        searcher = GridSearcher(parameters_grid['clustering'], save_path = save_path)
+        m, rmse, preds, instances = searcher.cv_clustering( gdf_train, gdf_val, gdf_test, max_iter, patience, area, threshold)
+
+        rg = Ridge
+        searcher = GridSearcher(parameters_grid['Lr_ridge'], save_path = save_path)
+        searcher.search(rg, df_train, df_val, df_test, 'Lr_ridge')
 
 
-rg = Ridge
-searcher = GridSearcher(parameters_grid['Lr_ridge'], save_path = save_path)
-searcher.search(rg, df_train, df_val, df_test, 'Lr_ridge')
 
-rg = RandomForestRegressor
-searcher = GridSearcher(parameters_grid['RDF'], save_path = save_path)
-searcher.search(rg, df_train, df_val, df_test, 'RDF')
+
+if __name__ == "__main__":
+    main(parameters_grid, citys, selected_areas, max_iter, patience)
